f63a01d9fa86de5263b34bcd4a3f264d86f40cd7
Saad E <21321555+se447@users.noreply.github.com>
2019-02-09 19:47:45 -0500

    Asked user to input radius and created equation to calculate circumference

--- src/Circumference.java
+++ src/Circumference.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 public class Circumference {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Program your solution here 
+        
+        //Program your solution here
+        System.out.print("Type the radius: "); //ask user to input radius
+        int radius = Integer.parseInt(reader.nextLine());
+        
+        double circumference = 2 * Math.PI * radius;
+        
+        System.out.println("Circumference of the circle: " + circumference);
     }
 }
